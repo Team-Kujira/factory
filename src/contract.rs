0c19821f68e5c9b419c0f8c0d7f91d2612ea6ce2
codehans <94654388+codehans@users.noreply.github.com>
2022-11-01 18:36:24 +0000

    Implementation

--- src/contract.rs
+++ src/contract.rs
@@ -1,35 +1,49 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-// use cw2::set_contract_version;
+use cosmwasm_std::{
+    Addr, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, StdResult, WasmMsg,
+};
+use cw_storage_plus::Item;
 
-use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
-/*
-// version info for migration info
-const CONTRACT_NAME: &str = "crates.io:kujira-factory";
-const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
+static ADMIN: Item<Option<Addr>> = Item::new("admin");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    _msg: InstantiateMsg,
-) -> Result<Response, ContractError> {
-    unimplemented!()
+    msg: InstantiateMsg,
+) -> StdResult<Response> {
+    ADMIN.save(deps.storage, &msg.admin)?;
+    Ok(Response::default())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _info: MessageInfo,
-    _msg: ExecuteMsg,
-) -> Result<Response, ContractError> {
-    unimplemented!()
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> StdResult<Response> {
+    match msg {
+        ExecuteMsg::InstantiateContract {
+            label,
+            code_id,
+            msg,
+        } => {
+            let admin = ADMIN.load(deps.storage)?;
+            let msg = CosmosMsg::Wasm(WasmMsg::Instantiate {
+                admin: admin.map(|a| a.to_string()),
+                code_id,
+                msg,
+                funds: info.funds,
+                label,
+            });
+            Ok(Response::default().add_message(msg))
+        }
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
--- src/msg.rs
+++ src/msg.rs
@@ -1,11 +1,23 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{Addr, Binary};
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub admin: Option<Addr>,
+}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    InstantiateContract {
+        label: String,
+        code_id: u64,
+        msg: Binary,
+    },
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    #[returns(Addr)]
+    Admin {},
+}
